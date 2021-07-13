b62b05edaeb010f88a4b88eb0c223a0e6aac5da6
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-07-13 15:41:34 -0400

    likes are functional

--- src/App.js
+++ src/App.js
@@ -30,11 +30,25 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(
+      posts.map((post) => {
+        if (post.id === postId) {
+          post.likes = post.likes + 1;
+          return post;
+        } else {
+          return post;
+        }
+      })
+    );
+
+    console.log(posts);
   };
 
   return (
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
